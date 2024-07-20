fd4d5c9c3792f77ef508b734c96234a26bdc419b
Shubham Mishra <meshuham@duck.com>
2024-07-20 23:46:16 +0530

    daya_6_crackyourinternship

---
+++ day_6_crackyourinternship/majority_element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> mpp;
+        int n= nums.size();
+        for(auto it: nums){
+            mpp[it]++;
+            if(mpp[it]>n/2){
+                return it;
+            }
+        }
+
+        return -1;
+    }
+};
